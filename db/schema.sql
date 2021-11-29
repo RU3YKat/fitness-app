a2d674a8033ce7643220af04954b0cc59de76a19
sunita persad <sunitapersad020@gmail.com>
2021-11-29 15:11:19 -0500

    create database

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,3 @@
 DROP DATABASE IF EXISTS calorie_tracker_db;
 CREATE DATABASE calorie_tracker_db;
-USE calorie_tracker_db;
 
-CREATE TABLE foods (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  meal VARCHAR(25),
-  calories INT
-);
