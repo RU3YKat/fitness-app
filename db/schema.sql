93fda9d619abe11c0fbf745ca39d9904a6b45529
insomniKat <ru3ykat@gmail.com>
2021-11-29 22:11:31 -0500

    rm unused code

--- db/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS calorie_tracker_db;
-CREATE DATABASE calorie_tracker_db;
-USE calorie_tracker_db;
-
-CREATE TABLE foods (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  meal VARCHAR(25),
-  calories INT
-);
