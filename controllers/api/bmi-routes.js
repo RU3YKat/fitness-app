aa2673039f5dd6a2a162d355830c5c40fba07104
sylverainv <sylverainv@gmail.com>
2021-12-01 01:49:31 -0500

    Updated routes, added front end

---
+++ controllers/api/bmi-routes.js
@@ -0,0 +1,74 @@
+const router = require('axios').Router();
+const { User } = require('../../models');
+
+//sends index.html
+router.get("/bmicalculator", function (req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
+});
+
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+    });
+});
+
+// get user by height
+router.get('/:height', (req, res) => {
+    User.findAll({
+        attributes: {exclude: ['password'] },
+        where: {
+            height: req.params.height
+            // [height.overlap]: [60, 70]
+        }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+// get user by weight
+router.get('/:weight', (req, res) => {
+    User.findAll({
+        attributes: {exclude: ['password'] },
+        where: {
+            weight: req.params.weight
+            // [weight.overlap]: [60, 70]
+        }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+// get user by age
+router.get('/:age', (req, res) => {
+    User.findAll({
+        attributes: {exclude: ['password'] },
+        where: {
+            age: req.params.age
+            // [age.overlap]: [60, 70]
+        }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+router.post('/',(req,res)=>{
+    User.create(req.body).then(dbUserData => res.json({message:"success"}))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+ 
+});
+module.exports = router;
