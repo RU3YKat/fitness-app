ef805b9248928f46b4566904e0981c5f6e2c1858
insomniKat <ru3ykat@gmail.com>
2021-12-01 18:10:44 -0500

    sequelize store updated

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ let exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// unused const at this file
-// const mysql = require('mysql');
-// const mysql2 = require('mysql2');
-
 const homeRoutes = require('./controllers/home-routes');
 
 const sequelize = require('./config/connection');
@@ -38,12 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // import routes and give server access to them
 app.use(require('./controllers'));
-
-// start the server listening on a port
-// app.listen(port, ()=>{
-//     console.log(`App is listening on PORT ${port}`);
-// })
-
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
