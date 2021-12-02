8aceaaba0b7fee9ceb76178908bf8e3904708dc1
insomniKat <ru3ykat@gmail.com>
2021-12-02 16:45:00 -0500

    presentation editing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+// const userRoutes = require('./user-routes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 
 const foodRoutes = require('./food-routes');
 
