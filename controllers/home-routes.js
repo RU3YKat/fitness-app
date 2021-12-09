2fad7325d814d65df2886331805540df31016d97
insomniKat <ru3ykat@gmail.com>
2021-12-09 16:48:24 -0500

    extended CRUD by FK pass

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,43 +12,43 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/index', async (req, res) => {
+// router.get('/index', async (req, res) => {
   
     // use the sequelize ORM method findAll to get back all the rows in the food table
     // each row becomes an object inside an array, meaning allFoods is an array
-    const getFoodsByProf = async () => {
-      const response = await fetch(`/api/profiles/${id}`, {
-        method: 'GET',
-        body: JSON.stringify({
+//     const getFoodsByProf = async () => {
+//       const response = await fetch(`/api/profiles/${id}`, {
+//         method: 'GET',
+//         body: JSON.stringify({
           
-        })
+//         })
+//       })
+//     }
+
+// })
+
+router.get('/index', async (req, res) => {
+    try {
+      // use the sequelize ORM method findAll to get back all the rows in the food table
+      // each row becomes an object inside an array, meaning allFoods is an array
+      const allFoods = await Food.findAll()
+      const newAllFoods = allFoods.map((food) => food.get({ plain: true }))
+        console.log(newAllFoods);
+        let calorieSum=0;
+        newAllFoods.forEach(food=>{
+          calorieSum += food.calories;
       })
+      console.log(calorieSum);
+        res.render('index', {items: newAllFoods, 
+          itemSum:calorieSum, loggedIn: req.session.loggedIn
+      })
+
     }
-
-})
-
-// router.get('/index', async (req, res) => {
-//     try {
-//       // use the sequelize ORM method findAll to get back all the rows in the food table
-//       // each row becomes an object inside an array, meaning allFoods is an array
-//       const allFoods = await Food.findAll()
-//       const newAllFoods = allFoods.map((food) => food.get({ plain: true }))
-//         console.log(newAllFoods);
-//         let calorieSum=0;
-//         newAllFoods.forEach(food=>{
-//           calorieSum += food.calories;
-//       })
-//       console.log(calorieSum);
-//         res.render('index', {items: newAllFoods, 
-//           itemSum:calorieSum, loggedIn: req.session.loggedIn
-//       })
-
-//     }
-//     catch (err) {
-//       // 500 communicates to client that there is something wrong with the website's servers
-//       res.status(500).json(err)
-//     }
-//   })
+    catch (err) {
+      // 500 communicates to client that there is something wrong with the website's servers
+      res.status(500).json(err)
+    }
+  })
 
 
 module.exports = router;
